5695255a5831a9f471d67eecddac7ca16bbf2d3c
moweather <zax41006@gmail.com>
2026-07-13 12:21:58 +0800

    Update TC_A.py

--- TC_A.py
+++ TC_A.py
@@ -55,7 +55,7 @@ def draw_chart():
             forecast_data.append([row['f_time'], row['lng'], row['lat'], row['wind'], h, row['minimum central pressure'], row.get('type', 'tropical')])
 
         fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
-        ax.set_extent([102.5, 127.5, 12.5, 32.5], crs=ccrs.PlateCarree())
+        ax.set_extent([107.0, 122.0, 14.5, 26.5], crs=ccrs.PlateCarree())
 
         ax.add_feature(cfeature.LAND, facecolor="#F5F5DC", edgecolor="#795548", linewidth=0.8, zorder=1)
         ax.add_feature(cfeature.OCEAN, facecolor="#E3F2FD", zorder=0)
