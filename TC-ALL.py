c1bc8f37d0b41adcfcdc39c19fd8f6c4424f2018
moweather <zax41006@gmail.com>
2026-08-09 16:58:32 +0800

    Update TC-ALL.py

--- TC-ALL.py
+++ TC-ALL.py
@@ -34,7 +34,7 @@ TC_LIST = [
         "fore_csv": os.path.join(BASE_PATH, "forecast_track_D.csv")
     },
     {
-        "name": "熱帶低氣壓",
+        "name": "琵鷺",
         "past_csv": os.path.join(BASE_PATH, "past_track_E.csv"),
         "fore_csv": os.path.join(BASE_PATH, "forecast_track_E.csv")
     },
