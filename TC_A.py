837dc9a44330b9487c66e380e64c6fb6791154e4
moweather <zax41006@gmail.com>
2026-08-22 09:57:11 +0800

    Update TC_A.py

--- TC_A.py
+++ TC_A.py
@@ -17,7 +17,7 @@ import tcmarkers
 
 # ==================== 1. 氣旋與 GitHub Pages 網址設定 ====================
 TC_ID = "A"
-TC_NAME = "未命名"
+TC_NAME = "紫檀"
 
 # 你的 GitHub Pages 基礎網址
 SITE_BASE_URL = "https://zax41006-bot.github.io/TC-Track"
